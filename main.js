9685e94d54466ff42fd629b05050c88e370bcf8f
Ana Carol Viana <54864592+AnaCarolViana@users.noreply.github.com>
2020-01-14 14:58:38 -0300

    Funcionalidades para reinicialização do programa

--- main.js
+++ main.js
@@ -1,15 +1,51 @@
-var frase = $(".fraseDesafio").text()
-var numeroPalavras = frase.split(" ").length
-var tamanhoFrase = $("#tamanhoFrase")
-tamanhoFrase.text(numeroPalavras);
+var tempoInicial = $(".tempoDigitacao").text()
+var campo = $(".campoDigitacao");
 
-var campo = $(".campoDigitacao")
-campo.on("input", function(){
+$(function(){
+    atualizaTamanhoFrase()
+    inicializaContadores()
+    incicializaCronometro()
+    $(".botaoReiniciar").click(reiniciaJogo)
+});
+
+function atualizaTamanhoFrase(){
+    var frase = $(".fraseDesafio").text()
+    var numeroPalavras = frase.split(" ").length
+    var tamanhoFrase = $("#tamanhoFrase")
+        tamanhoFrase.text(numeroPalavras)
+};
+
+function inicializaContadores(){
+        campo.on("input", function(){
     var conteudo = campo.val()
     var qtdPalavras = conteudo.split(/\S+/).length-1
-    $(".contadorPalavras").text(qtdPalavras);
-
+        $(".contadorPalavras").text(qtdPalavras);
+    
     var qtdCaracteres = conteudo.length
-    $(".contadorCaracteres").text(qtdCaracteres);
+        $(".contadorCaracteres").text(qtdCaracteres)
+    
+    })
+};
 
-});
+function incicializaCronometro(){
+    var tempoRestante = $(".tempoDigitacao").text()
+        campo.one("focus", function(){
+    var cronometro = setInterval(function(){
+        tempoRestante--
+            $(".tempoDigitacao").text(tempoRestante)
+        if(tempoRestante < 1){ 
+            campo.attr("disabled", true)
+                clearInterval(cronometro)
+            }
+        },1000)
+    })
+};
+
+function reiniciaJogo(){
+    campo.attr("disabled", false)
+    campo.val("")
+    $(".contadorPalavras").text("0")
+    $(".contadorCaracteres").text("0")
+    $(".tempoDigitacao").text(tempoInicial)
+    incicializaCronometro()
+};
